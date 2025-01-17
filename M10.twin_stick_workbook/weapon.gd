662f991703e0577c3d1e930c4dd4b73fc9caf623
Tony Kuang <159198066+Tonykuang1028@users.noreply.github.com>
2025-01-17 15:48:10 -0800

    aiming and shooting

--- M10.twin_stick_workbook/weapon.gd
+++ M10.twin_stick_workbook/weapon.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
-@export var bullet_scene: PackedScene = preload("res://levels/bullet.tscn")
+#@export var bullet_scene: PackedScene = preload("res://levels/bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	
-func shoot() -> void:
-	var bullet: Node = bullet_scene.instantiate()
-	get_tree().current_scene.add_child(bullet)
+#func shoot() -> void:
+	#var bullet: Node = bullet_scene.instantiate()
+	#get_tree().current_scene.add_child(bullet)
 	
-
+	#bullet.global_positon = global_position
+	#bullet.global_rotation = global_rotation
+	#bullet.rotation += randf_range(-random_angle / 2.0, random_angle / 2.0)
 	
+func shoot():
+	const BULLET = preload("res://levels/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = $shootleft.global_position
+	$shootleft.add_child(new_bullet)
