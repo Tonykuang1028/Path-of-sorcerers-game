f2f4dfe38a712e33e3775e9300b520f59131cd64
Tony Kuang <159198066+Tonykuang1028@users.noreply.github.com>
2025-02-03 15:51:50 -0800

    item animation

--- M10.twin_stick_workbook/pickups/health_pack.gd
+++ M10.twin_stick_workbook/pickups/health_pack.gd
@@ -4,24 +4,30 @@ class_name Pickup extends Area2D
 @export var item: Item = null: set = set_item
 
 @onready var _sprite_2d: Sprite2D = %Sprite2D
-@onready var _audio_stream_player: AudioStreamPlayer2D = %AudioStreamPlayer2D
-@onready var _animation_player: AnimationPlayer = %AnimationPlayer
+@onready var _audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
 	set_item(item)
 
+	_animation_player.play("idle")
 	
 	body_entered.connect(func (body: Node2D) -> void:
 		if body is Player:
 			item.use(body)
-		
-	
-		
-		
+		_animation_player.play("destroy")
+		set_deferred("monitoring", false)
+		_audio_stream_player_2d.play()
+		_animation_player.animation_finished.connect(func (_animation_name: String) -> void:
 			queue_free()
 		)
-
+	)
+		
+		
+			
+	
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
@@ -34,5 +40,5 @@ func set_item(value: Item) -> void:
 	item = value
 	if _sprite_2d != null:
 		_sprite_2d.texture = item.texture
-	if _audio_stream_player != null:
-		_audio_stream_player.stream = item.sound_on_pickup
+	if _audio_stream_player_2d != null:
+		_audio_stream_player_2d.stream = item.sound_on_pickup
